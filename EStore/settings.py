9958cbb505e01c9c180816e8d0846d5d5be9aa6a
TranHoangDiep <66518630+TranHoangDiep@users.noreply.github.com>
2023-03-24 15:05:07 +0700

    Update settings.py

--- EStore/settings.py
+++ EStore/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'EStore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'postgresql',
+        'PASSWORD': '12345678',
+        'HOST': 'database.cmrpkytu2z7y.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
