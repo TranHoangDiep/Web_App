58963215d4804ebe7f2c49887df94cd5b00a2c93
tranhoangdiepbp@gmail.com <66518630+TranHoangDiep@users.noreply.github.com>
2023-03-24 15:11:38 +0700

    first commit

--- EStore/settings.py
+++ EStore/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'EStore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'postgresql',
+        'PASSWORD': '12345678',
+        'HOST': 'database.cmrpkytu2z7y.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
